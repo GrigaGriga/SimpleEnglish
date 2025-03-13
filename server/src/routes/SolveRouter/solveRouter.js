077a733e113eb7eac7a8d8db40c49253fc41000f
GrigaGriga <oleggrigonis@gmail.com>
2025-03-13 15:33:29 +0700

    add words2

--- server/src/routes/SolveRouter/solveRouter.js
+++ server/src/routes/SolveRouter/solveRouter.js
@@ -1,15 +1,16 @@
 const solveRouter = require('express').Router();
-const SolveService = require('../../services/WordsServices/WordsServices');
+const SolveServices = require('../../services/SolveServices/SolveServices');
 // const checkId = require('../../middlewares/checkId');
 // const { verifyAccessToken } = require('../../middlewares/verifyTokens');
 
 solveRouter.route('/').post(async (req, res) => {
-  console.log(req.body)
+  // console.log(req.body)
+  const {wordId, userId} = req.body
   // const cardId = Number(req.params.id);
   try {
-    const wordsArr = await SolveService.addSolve();
-    // console.log(wordsArr.map(el=>el.get()));
-    // res.json(wordsArr.map(el=>el.get()));
+    const solve = await SolveServices.addSolve(wordId, userId);
+    console.log(solve);
+    res.status(201).json(solve);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: 'Ошибка сервера' });
